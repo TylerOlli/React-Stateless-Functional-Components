58d8f6eb31dbacc88b65d4f66b2cfde94b0866ed
TylerOlli <ollityler@gmail.com>
2020-04-15 17:05:36 -0400

    Removed un-needed comments

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const MovieCard = props => {
-    /*
-    Destructuring via ES6. We're getting the profiles, users, and movies properties
-    off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-    out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-    */
+
     const { users, usersWhoLikedMovie, movieInfo } = props;
 
     return (
--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -2,11 +2,6 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 const MovieCardsList = props => {
-    /*
-  Destructuring via ES6. We're getting the profiles, users, and movies properties
-  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-  */
 
     const { profiles, users, movies } = props;
     const usersByMovie = {};
